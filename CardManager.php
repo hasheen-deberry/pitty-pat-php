bc075c53ad413adbc73a06a00cba362b41c61861
hasheen-deberry <deberryh@gmail.com>
2018-03-11 22:04:43 -0400

    starting to add test files for TDD. Still have to refactor hand managers

--- CardManager.php
+++ CardManager.php
@@ -1,5 +1,15 @@
 <?php
 
-class cardManager{
+include CardDeck.php;
 
+class CardManager{
+	private $_playerOne; //CardManager object
+	private $_playerTwo; //CardManager object
+	private $_deck;
+
+	function __construct(&playerOne, &playerTwo){
+		$this->_playerOne = $playerOne;
+		$this->_playerTwo = $playerTwo;
+		$this->_deck = new CardDeck();
+	}
 }
---
+++ Tests/CardManagerTests.php
@@ -0,0 +1,23 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class CardManagerTests extends TestCase{
+	
+	protected function setUp(){
+
+	}
+
+	protected function tearDown(){
+
+	
+	}
+
+	function NewCardManager_ExpectDeckCountToBeFiftyTwo(){
+
+	}
+
+	function NewCardManager_DealCards_ExpectDeckCountToBeFortyOne(){
+
+	}
+}
